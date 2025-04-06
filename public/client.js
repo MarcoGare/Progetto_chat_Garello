8f0a93308417a8d54c89d08860e3f5831f097e9b
Marco Garello <garellomarco@itis-molinari.eu>
2025-04-06 21:18:14 +0200

    Client completo

--- public/client.js
+++ public/client.js
@@ -13,3 +13,49 @@ const socket = io();
 
 const mostraModale = new bootstrap.Modal(document.getElementById("modaleNome"));
 mostraModale.show();
+
+bottoneNome.onclick = () => {
+  const nome = campoNome.value;
+  socket.emit("nome", nome);
+  mostraModale.hide();
+}
+
+campoMessaggio.onkeydown = (evento) => {
+  if (evento.keyCode === 13) {
+    evento.preventDefault();
+    bottoneInvia.click();
+  }
+}
+
+bottoneInvia.onclick = () => {
+  socket.emit("messaggio", campoMessaggio.value);
+  campoMessaggio.value = "";
+}
+
+socket.on("chat", (messaggio) => {
+  messaggi.push(messaggio);
+  disegnaChat();
+});
+
+socket.on("lista", (lista) => {
+  disegnaLista(lista);
+});
+
+const disegnaChat = () => {
+  let html = "";
+  messaggi.forEach((m) => {
+    const riga = modelloMessaggio.replace("%MESSAGGIO", m);
+    html += riga;
+  });
+  chat.innerHTML = html;
+  window.scrollTo(0, document.body.scrollHeight);
+}
+
+const disegnaLista = (lista) => {
+  let html = "";
+  lista.forEach((u) => {
+    const riga = modelloUtente.replace("%NOME", u.nome);
+    html += riga;
+  });
+  utenti.innerHTML = html;
+}
